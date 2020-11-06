a612607e3515fde9ac2f4b6f4b0793d23cc0f50e
zhang zuqi <361267211@qq.com>
2020-11-06 20:30:00 +0800

    添加项目文件。

---
+++ ShuJuJieGou-Study/YanShiChuangTi/YanShiChuangTi/Class/BST.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Text;
+
+namespace ShuJuJieGouStudy
+{
+    class BST<E> where E : IComparable
+    {
+        public class Node
+        {
+
+            public E e;
+            public Node left, right;
+            public Node(E e)
+            {
+                this.e = e;
+                left = null;
+                right = null;
+            }
+        }
+
+        private Node root;
+        private int size;
+        public BST()
+        {
+            root = null;
+            size = 0;
+        }
+        public int GetSize()
+        {
+            return size;
+        }
+        public void Add(E e)
+        {
+            root = Add(root, e);
+        }
+        private Node Add(Node node,E e)
+        {
+             if (node==null)
+            {
+               Node ret= new Node(e);
+                size++;
+                return ret;
+            }
+            
+
+            if (e.CompareTo(node.e)<0)
+            {
+               node.left= Add(node.left, e);
+                
+            }
+           else if (e.CompareTo(node.e) > 0 )
+            {
+                node.right = Add(node.right, e);
+            }
+           
+                return node;
+           
+        }
+
+        public bool Contains(E e)
+        {
+            bool isContains=false;
+           return Contains(root, e);
+        }
+        public bool Contains(Node node,E e)
+        {
+            if (node==null)
+            {
+                return false;
+            }
+            if (e.CompareTo(node.e)==0)
+            {
+                return true;
+            }
+            else if(e.CompareTo(node.e) < 0)
+            {
+                return Contains(node.left, e);
+            }
+            else 
+            {
+                return Contains(node.right, e);
+            }
+
+        }
+        /// <summary>
+        /// 最常用
+        /// </summary>
+        public void PreOrder()
+        {
+            PreOrder(root);
+        }
+        public void PreOrder(Node node)
+        {
+            if (node==null)
+            {
+                return;
+            }
+
+            Console.WriteLine(node.e.ToString());
+            PreOrder(node.left);
+            PreOrder(node.right);
+        }
+        /// <summary>
+        /// 返回有序的结果
+        /// </summary>
+        public void InOrder()
+        {
+            InOrder(root);
+        }
+        public void InOrder(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            InOrder(node.left);
+            Console.WriteLine(node.e.ToString());
+            InOrder(node.right);
+        }
+        /// <summary>
+        /// 优先处理孩子节点
+        /// </summary>
+        public void PostOrder()
+        {
+            PostOrder(root);
+        }
+        public void PostOrder(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            PostOrder(node.left);            
+            PostOrder(node.right);
+            Console.WriteLine(node.e.ToString());
+        }
+    }
+}
---
+++ ShuJuJieGou-Study/YanShiChuangTi/YanShiChuangTi/TestForm.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ShuJuJieGouStudy;
+
+
+namespace ShuJuJieGouStudy
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                linkedList.AddFirst(i);
+                Console.WriteLine(linkedList.ToString());
+            }
+            linkedList.Add(555, 2);
+            Console.WriteLine(linkedList.ToString());
+
+            linkedList.Remove( 2);
+            Console.WriteLine(linkedList.ToString());
+        }
+
+        private void btn_LinkedListStack_Click(object sender, EventArgs e)
+        {
+            LinkedListStack<int> linkedListStack = new LinkedListStack<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                linkedListStack.Push(i);
+                Console.WriteLine(linkedListStack.ToString());
+            }
+
+            linkedListStack.Pop();
+            Console.WriteLine(linkedListStack.ToString());
+        }
+
+        private void btn_Array_Click(object sender, EventArgs e)
+        {
+            Array<String> arr = new Array<String>(5);
+            for (int i = 0; i < 10; i++)
+            {
+                arr.AddLast(i.ToString() + "个人");
+                Console.WriteLine(arr.ToString());
+            }
+
+
+            arr.Add(1, 100.ToString() + "个人");
+            Console.WriteLine(arr.ToString());
+
+
+
+            for (int i = 0; i < 11; i++)
+            {
+                arr.RemoveLast();
+                Console.WriteLine(arr.ToString());
+            }
+        }
+
+        private void btn_ArrayQuene_Click(object sender, EventArgs e)
+        {
+            ArrayQuene<int> arrayQuene = new ArrayQuene<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                arrayQuene.Enquene(i + 1);
+                Console.WriteLine(arrayQuene.ToString());
+                if (i % 3 == 2)
+                {
+                    arrayQuene.Dequene();
+                    Console.WriteLine(arrayQuene.ToString());
+                }
+            }
+
+        }
+
+        private void btn_ArrayStack_Click(object sender, EventArgs e)
+        {
+            ArryStack<int> arryStack = new ArryStack<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                arryStack.Push(i);
+                Console.WriteLine(arryStack.ToString());
+            }
+
+            arryStack.Pop();
+            Console.WriteLine(arryStack.ToString());
+        }
+        
+
+        private void btn_KuoHaoPiPei_Click(object sender, EventArgs e)
+        {
+            bool b = IsVailed(textBox1.Text);
+            if (!b)
+            {
+                MessageBox.Show("不匹配");
+            }
+        }
+
+        private bool IsVailed(string str)
+        {
+            ArryStack<char> arryStack = new ArryStack<char>();
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (str[i] == '{' || str[i] == '[' || str[i] == '(')
+                {
+                    arryStack.Push(str[i]);
+                }
+                else if (str[i] == '}' || str[i] == ']' || str[i] == ')')
+                {
+                    if (arryStack.IsEmpty() == true)
+                    {
+                        return false;
+                    }
+                    else if (arryStack.Peek() == '{')
+                    {
+                        if (str[i] == '}')
+                        {
+                            arryStack.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else if (arryStack.Peek() == '[')
+                    {
+                        if (str[i] == ']')
+                        {
+                            arryStack.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else if (arryStack.Peek() == '(')
+                    {
+                        if (str[i] == ')')
+                        {
+                            arryStack.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return arryStack.IsEmpty();
+
+        }
+    }
+}
